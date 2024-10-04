17e89cef4e62e32739dbcdf5bf19ac2988dff3fe
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-10-04 17:44:01 +0400

    V2 finish

--- scripts.js
+++ scripts.js
@@ -43,8 +43,14 @@ fetch("https://discord.com/api/guilds/1273294291518619749/widget.json").then((d)
 
     } else if (user.presence_count == 1) {
         const imgAvatar = document.getElementById("imgAvatar")
-        const Avatar = document.createElement("img")
-        Avatar.setAttribute("src", "img/ryan.png")
+        const Main_avatar = document.getElementById("Main_avatar")
+
+        const Avatar1 = document.createElement("img")
+        const Avatar2 = document.createElement("img")
+
+        Avatar1.setAttribute("src", "img/ryan.png")
+        Avatar2.setAttribute("src", "img/ryan.png")
+
         const userName = document.getElementById("userName")
         const Name = document.createElement("h2")
         const nameCheck = (name) => {
@@ -64,16 +70,19 @@ fetch("https://discord.com/api/guilds/1273294291518619749/widget.json").then((d)
             }
         }
         Name.innerHTML = nameCheck(user.members[0].username)
-        imgAvatar.appendChild(Avatar)
+        imgAvatar.appendChild(Avatar1)
+        Main_avatar.appendChild(Avatar2)
+
         userName.appendChild(Name)
 
     }
 
 })
-function play(){
-  const play = document.getElementById("play")
-  const myVideo = document.getElementById("myVideo")
-  play.style.display = "none"
-  myVideo.play()
-  myVideo.onload()
+function play() {
+    const audio = document.getElementById("audio")
+    const play = document.getElementById("play")
+    const myVideo = document.getElementById("myVideo")
+    play.style.display = "none"
+    myVideo.play()
+    audio.play()
 }
