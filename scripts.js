871758f92e7aa58a86261fef965189341644832f
datodato788 <151830320+datodato788@users.noreply.github.com>
2024-10-21 18:52:54 +0400

    V4

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 fetch("https://discord.com/api/guilds/1273294291518619749/widget.json").then((d) => { return d.json() }).then((user) => {
 
 
+    const status = document.getElementById("status")
 
     if (user.presence_count == 2) {
 
         const imgAvatar = document.getElementById("imgAvatar")
         const Avatar1 = document.createElement("img")
+        Avatar1.classList.add("cardImgs")
         Avatar1.setAttribute("src", user.members[1].avatar_url)
         const Avatar2 = document.createElement("img")
         Avatar2.setAttribute("src", user.members[1].avatar_url)
@@ -14,7 +16,24 @@ fetch("https://discord.com/api/guilds/1273294291518619749/widget.json").then((d)
         // -
         const Main_avatar = document.getElementById("Main_avatar")
         Main_avatar.appendChild(Avatar2)
+        // -
+        if (user.members[1].status == "online") {
+            status.innerHTML = '<img class="online" src="img/online.png" alt="online">'
+            Avatar2.classList.add("onlineC")
 
+
+
+        } else if (user.members[1].status == "idle") {
+            status.innerHTML = '<img class="idle" src="img/idle.png" alt="idle">'
+
+            Avatar2.classList.add("idleC")
+
+        } else if (user.members[1].status == "dnd") {
+            status.innerHTML = ' <img class="dnd" src="img/dnd.png" alt="dnd">'
+            Avatar2.classList.add("dndC")
+
+
+        }
 
 
         // -----------------------------------
@@ -44,10 +63,16 @@ fetch("https://discord.com/api/guilds/1273294291518619749/widget.json").then((d)
         const Main_avatar = document.getElementById("Main_avatar")
 
         const Avatar1 = document.createElement("img")
+        Avatar1.classList.add("cardImgs")
+
         const Avatar2 = document.createElement("img")
 
         Avatar1.setAttribute("src", "img/ryan.png")
         Avatar2.setAttribute("src", "img/ryan.png")
+
+        status.innerHTML = '<img class="offline" src="img/offline.png" alt="offline" />'
+        Avatar2.classList.add("offlineC")
+
 
         const userName = document.getElementById("userName")
         const Name = document.createElement("h2")
